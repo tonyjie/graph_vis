e4b0e42c64ae1bce5802ff357117d3217ddc8ef2
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2022-06-28 18:03:04 -0500

    Cleaned up dataloader code

--- batch_sgd.py
+++ batch_sgd.py
@@ -20,8 +20,6 @@ class odgiDataset(torch.utils.data.Dataset):
         self.g = odgi.graph()
         self.g.load(file_name)
 
-        print("Min node id: ", self.g.min_node_id())
-        print("Max node id: ", self.g.max_node_id())
         assert self.g.min_node_id() == 1
         assert self.g.max_node_id() == self.g.get_node_count()
 
@@ -31,79 +29,39 @@ class odgiDataset(torch.utils.data.Dataset):
         self.sizes = []
         self.g.for_each_path_handle(lambda p: self.sizes.append(self.g.get_step_count(p)))
 
-        self.ranges = []
-        for idx, s in enumerate(self.sizes):
-            combs = int(s * (s-1) / 2)
-            if idx > 0:
-                prev = self.ranges[idx-1]
-                self.ranges.append(self.ranges[idx-1] + combs)
-            else:
-                self.ranges.append(combs)
-
-        self.w_max = 1                          # w_max when nodes next to each other
-        self.w_min = 1 / ((max(self.sizes)-1)**2)    # w_min when nodes on ends of longest path
-
-        self.rnd_path_analysis = [0]*self.get_path_count()
-        self.rnd_node_analysis = [0]*self.get_node_count()
-        print(self.rnd_path_analysis)
-        print(self.rnd_node_analysis)
-
+        self.w_max = 1                                  # w_max when nodes next to each other
+        self.w_min = 1 / ((max(self.sizes)-1)**2)       # w_min when nodes on ends of longest path
         return
 
     def __getitem__(self, index): # return i, j, w, dis[i, j]
-        # # get path and compute combination index in path
-        # path = None
-        # comb_idx = index
-        # for (path_idx, r) in enumerate(self.ranges):
-        #     if index < r:
-        #         path = self.get_path(path_idx)
-        #         if path_idx > 0:
-        #             comb_idx = comb_idx - self.ranges[path_idx-1]
-        #         print("path-index: ", path_idx)
-        #         print("comb-index: ", comb_idx)
-        #         break
-
-
-        # random.seed(index)
-
         # choose random path
+        # utilization of index for finding specific node combination would be compute intensive
         path_idx = random.randrange(self.g.get_path_count())
         path = self.get_path(path_idx)
 
         # choose random step_a and step_b in path
         path_size = self.g.get_step_count(path)
-        step_a_idx = random.randrange(path_size - 1)
-        step_b_idx = random.randrange(step_a_idx + 1, path_size)
-        assert step_a_idx < step_b_idx
+        idx1 = random.randrange(path_size)
+        idx2 = random.randrange(path_size)
+        while (idx1 == idx2):
+            idx2 = random.randrange(path_size)
+        if idx1 < idx2:
+            idx_step_a = idx1
+            idx_step_b = idx2
+        else:
+            idx_step_a = idx2
+            idx_step_b = idx1
+        assert idx_step_a < idx_step_b
 
-        d = step_b_idx - step_a_idx
+        d = idx_step_b - idx_step_a
         w = 1/(d**2)
 
-        # get id of step_a
-        step = self.g.path_begin(path)
-        for i in range(step_a_idx):
-            assert self.g.has_next_step(step) == True
-            step = self.g.get_next_step(step)
-
-        id_step_a = self.g.get_id(self.g.get_handle_of_step(step))
-
-        # get id of step_b
-        for i in range(d):
-            assert self.g.has_next_step(step) == True
-            step = self.g.get_next_step(step)
-
-        id_step_b = self.g.get_id(self.g.get_handle_of_step(step))
-
-        self.rnd_path_analysis[path_idx] = self.rnd_path_analysis[path_idx] + 1
-        self.rnd_node_analysis[id_step_a-1] = self.rnd_node_analysis[id_step_a-1] + 1
-        self.rnd_node_analysis[id_step_b-1] = self.rnd_node_analysis[id_step_b-1] + 1
-
+        # get ids of step_a and step_b
+        [id_step_a, id_step_b] = self.get_node_ids_in_path(path, [idx_step_a, idx_step_b])
         return id_step_a, id_step_b, w, d
 
     def __len__(self):
-        # return self.ranges[self.g.get_path_count()-1]
-        return self.get_path_count() * self.get_node_count() * 10
-        # return self.get_nbr_nodes() * (self.get_nbr_nodes()-1) * 2
+        return sum(self.sizes) * 10
 
     def get_path(self, idx):
         assert 0 <= idx < self.g.get_path_count()
@@ -116,41 +74,55 @@ class odgiDataset(torch.utils.data.Dataset):
     def get_path_count(self):
         return self.g.get_path_count()
 
-    def get_node_id_in_path(self, path, sidx):
+    def get_node_ids_in_path(self, path, sidx_list):
+        assert isinstance(sidx_list, list)
+        prev_idx = None
+        ids = []
         step = self.g.path_begin(path)
-        for i in range(sidx):
-            assert self.g.has_next_step(step) == True
-            step = self.g.get_next_step(step)
+        for sidx in sidx_list:
+            delta = sidx
+            if prev_idx != None:
+                delta = delta - prev_idx
+            prev_idx = sidx
 
-        return self.g.get_id(self.g.get_handle_of_step(step))
+            for i in range(delta):
+                assert self.g.has_next_step(step) == True
+                step = self.g.get_next_step(step)
+            ids.append(self.g.get_id(self.g.get_handle_of_step(step)))
+        return ids
 
-    def get_step_cnt_in_path(self, path):
+    def get_step_count_in_path(self, path):
         return self.g.get_step_count(path)
 
 
 
 
 def draw_svg(x, y, gdata, output_name):
-    # Draw SVG Graph
-    # plt.axis('equal')
+    # Draw SVG Graph with edge
     ax = plt.axes()
-    ax.set_xlim(min(x), max(x))
-    ax.set_ylim(min(y), max(y))
+    min_x = min(x)
+    max_x = max(x)
+    edge_x = 0.1 * (max_x - min_x)
+    min_y = min(y)
+    max_y = max(y)
+    edge_y = 0.1 * (max_y - min_y)
+    ax.set_xlim(min_x-edge_x, max_x+edge_x)
+    ax.set_ylim(min_y-edge_y, max_y+edge_y)
 
     lines = []
-    for pidx in range(gdata.get_path_count()): #NOTE: Draw all paths
+    for pidx in range(gdata.get_path_count()):
         print("drawing path ", pidx)
         p = gdata.get_path(pidx)
 
         i = None
         j = None
-        for sidx in range(gdata.get_step_cnt_in_path(p)):
+        for sidx in range(gdata.get_step_count_in_path(p)):
             i = j
-            j = gdata.get_node_id_in_path(p, sidx)
+            [j] = gdata.get_node_ids_in_path(p, [sidx])
             if i != None:
                 lines.append([[x[i-1],y[i-1]], [x[j-1],y[j-1]]])
 
-    lc = mc.LineCollection(lines, linewidths=1, colors='k', alpha=.5)
+    lc = mc.LineCollection(lines, linewidths=1, alpha=.5)
     ax.add_collection(lc)
 
     plt.plot(x,y,marker='o', linestyle='', color='black')
@@ -179,20 +151,7 @@ def q(t1, t2, dis):
 
 
 def main(args):
-    # # ========== Load Graph, Get the Ground Truth (shortest distance here) ============
-    # graph_name = 'qh882'
-    # mat_data = io.loadmat(graph_name + '.mat')
-    # graph = mat_data['Problem']['A'][0][0]
-
-    # # find the shortest paths using dijkstra's
-    # # note: if any path length is infinite i.e. we have disconnected subgraphs,
-    # #       then this cell will work but the rest of the script won't.
-    # d = csgraph.shortest_path(graph, directed=False, unweighted=True)
-    # # print(f"d.shape: {d.shape}") # (882, 882)
-    # # print(f"d: {d}")
-    # n = d.shape[0] # number of nodes
-
-    # ========== Get Constraints and Input Data ============
+    # # ========== Load Graph, Get Constraints ============
     dataset = odgiDataset(args.file)
 
     n = dataset.get_node_count()
@@ -229,19 +188,15 @@ def main(args):
     # stress = compute_stress(positions, d)
     # print(f"initial stress: {stress}")
 
-    my_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+    my_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
     # ========== Training ============
     start = datetime.datetime.now()
-
-    rnd_node_analysis_2 = [0]*dataset.get_node_count()
     for idx_c, c in enumerate(schedule):
         lr = 0.4
         loss_fn = torch.nn.MSELoss(reduction='mean')
         optimizer = torch.optim.SGD([x,y], lr=lr)
         for batch_idx, (i, j, w, dis) in enumerate(my_dataloader): # batch size = 2
-            print(batch_idx, " / ", dataset.__len__())
-            rnd_node_analysis_2[i-1] = rnd_node_analysis_2[i-1] + 1
-            rnd_node_analysis_2[j-1] = rnd_node_analysis_2[j-1] + 1
+            print(idx_c, ": ", batch_idx, " / ", dataset.__len__())
 
             dx = x[i-1] - x[j-1]
             dy = y[i-1] - y[j-1]
@@ -283,13 +238,8 @@ def main(args):
     # print(f"x_np.shape: {x_np.shape}")
     # print(f"x_np: {x_np}")
 
-    # NOTE: why is the result so wrong?
     draw_svg(x_np, y_np, dataset, f"out")
 
-    print(dataset.rnd_path_analysis)
-    print(dataset.rnd_node_analysis)
-    print(rnd_node_analysis_2)
-    
     # Compute the Total Stress
     # stress_total = compute_stress(x_np, d)
     # print(f"stress_total: {stress_total}")
@@ -299,7 +249,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Batch SGD Implementation for Graph Drawing")
     parser.add_argument('--batch_size', type=int, default=1, help='batch size')
     parser.add_argument('--num_iter', type=int, default=15, help='number of iterations')
-    parser.add_argument('--file', type=str, default='DRB1-3123.og', help='odgi variation graph')
+    parser.add_argument('--file', type=str, default='tiny_pangenome.og', help='odgi variation graph')
     args = parser.parse_args()
     print(args)
     main(args)
