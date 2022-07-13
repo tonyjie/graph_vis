efe5cf54eb669947529e0fe6c13ce509e1ded2db
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2022-07-13 17:53:19 -0500

    Moved from legacy odgi interface to current odgi_ffi

--- batch_sgd.py
+++ batch_sgd.py
@@ -1,7 +1,7 @@
 # Use with odgi branch zhang_research_extended https://github.com/nsmlzl/odgi/tree/zhang_research_extended
 # Run with command 'env LD_PRELOAD=libjemalloc.so.2 PYTHONPATH=<lib dir of odgi-build> python3 batch_sgd.py --batch_size 1 --num_iter 15'
 
-import odgi
+from odgi_ffi import *
 
 import torch
 import scipy.sparse.csgraph as csgraph
@@ -20,67 +20,66 @@ import argparse
 
 class odgiDataset(torch.utils.data.Dataset):
     def __init__(self, file_name):
-        self.g = odgi.graph()
-        self.g.load(file_name)
+        self.g = odgi_load_graph(file_name)
 
-        assert self.g.min_node_id() == 1
-        assert self.g.max_node_id() == self.g.get_node_count()
+        assert odgi_min_node_id(self.g) == 1
+        assert odgi_max_node_id(self.g) == self.get_node_count()
 
-        self.rnd_node_gen = odgi.RndNodeGenerator(self.g)
+        self.rnd_node_gen = odgi_create_rnd_node_generator(self.g)
 
         self.path_names = []
-        self.g.for_each_path_handle(lambda p: self.path_names.append(self.g.get_path_name(p)))
+        odgi_for_each_path_handle(self.g, lambda p: self.path_names.append(odgi_get_path_name(self.g, p)))
 
         self.sizes = []
-        self.g.for_each_path_handle(lambda p: self.sizes.append(self.g.get_step_count(p)))
+        odgi_for_each_path_handle(self.g, lambda p: self.sizes.append(odgi_get_step_in_path_count(self.g, p)))
 
         self.w_max = 1                                  # w_max when nodes next to each other
         self.w_min = 1 / ((max(self.sizes)-1)**2)       # w_min when nodes on ends of longest path
         return
 
     def __getitem__(self, index): # return i, j, w, dis[i, j]
-        node_pack = self.rnd_node_gen.get_random_nodes_pack()
-        id_node_a = node_pack.get_id_n0()
-        id_node_b = node_pack.get_id_n1()
-        d = float(node_pack.get_distance())
-        w = 1.0/(d**2)
-        vis_p_a = node_pack.get_vis_p_n0()
-        vis_p_b = node_pack.get_vis_p_n1()
+        node_pack = odgi_get_random_node_pack(self.rnd_node_gen)
+        id_node_a = odgi_RNP_get_id_n0(node_pack)
+        id_node_b = odgi_RNP_get_id_n1(node_pack)
+        d = float(odgi_RNP_get_distance(node_pack))
+        w = 1.0/float(d**2)
+        vis_p_a = odgi_RNP_get_vis_p_n0(node_pack)
+        vis_p_b = odgi_RNP_get_vis_p_n1(node_pack)
         return id_node_a, id_node_b, vis_p_a, vis_p_b, w, d
 
     def __len__(self):
         return sum(self.sizes) * 10         # similar to odgi default
 
     def get_path(self, idx):
-        assert 0 <= idx < self.g.get_path_count()
+        assert 0 <= idx < self.get_path_count()
         path_name = self.path_names[idx]
-        return self.g.get_path_handle(path_name)
+        return odgi_get_path_handle(self.g, path_name)
 
     def get_node_count(self):
-        return self.g.get_node_count()
+        return odgi_get_node_count(self.g)
 
     def get_path_count(self):
-        return self.g.get_path_count()
+        return odgi_get_path_count(self.g)
 
-    def get_node_ids_in_path(self, path, sidx_list):
-        assert isinstance(sidx_list, list)
-        prev_idx = None
-        ids = []
-        step = self.g.path_begin(path)
-        for sidx in sidx_list:
-            delta = sidx
-            if prev_idx != None:
-                delta = delta - prev_idx
-            prev_idx = sidx
+    # def get_node_ids_in_path(self, path, sidx_list):
+    #     assert isinstance(sidx_list, list)
+    #     prev_idx = None
+    #     ids = []
+    #     step = self.g.path_begin(path)
+    #     for sidx in sidx_list:
+    #         delta = sidx
+    #         if prev_idx != None:
+    #             delta = delta - prev_idx
+    #         prev_idx = sidx
 
-            for i in range(delta):
-                assert self.g.has_next_step(step) == True
-                step = self.g.get_next_step(step)
-            ids.append(self.g.get_id(self.g.get_handle_of_step(step)))
-        return ids
+    #         for i in range(delta):
+    #             assert self.g.has_next_step(step) == True
+    #             step = self.g.get_next_step(step)
+    #         ids.append(self.g.get_id(self.g.get_handle_of_step(step)))
+    #     return ids
 
-    def get_step_count_in_path(self, path):
-        return self.g.get_step_count(path)
+    # def get_step_count_in_path(self, path):
+    #     return self.g.get_step_count(path)
 
 
 
@@ -107,15 +106,15 @@ def draw_svg(x, gdata, output_name):
 
 
 
-def compute_stress(vis_pos, real_dis):
-    # compute the stress function
-    n = vis_pos.shape[0]
-    stress = 0
-    for i in range(n):
-        for j in range(i):
-            w = 1 / (real_dis[i,j]**2)
-            stress += w * (np.linalg.norm(vis_pos[i] - vis_pos[j]) - real_dis[i, j]) ** 2
-    return stress
+# def compute_stress(vis_pos, real_dis):
+#     # compute the stress function
+#     n = vis_pos.shape[0]
+#     stress = 0
+#     for i in range(n):
+#         for j in range(i):
+#             w = 1 / (real_dis[i,j]**2)
+#             stress += w * (np.linalg.norm(vis_pos[i] - vis_pos[j]) - real_dis[i, j]) ** 2
+#     return stress
 
 def q(t1, t2, dis):
     # one term in the stress function
@@ -196,7 +195,7 @@ def main(args):
     # print(f"x_np.shape: {x_np.shape}")
     # print(f"x_np: {x_np}")
 
-    odgi.generate_layout_file(x_np, "output/" + args.file + ".lay", dataset.g)
+    odgi_generate_layout_file(dataset.g, x_np, "output/" + args.file + ".lay")
     draw_svg(x_np, dataset, f"out")
 
     # Compute the Total Stress
