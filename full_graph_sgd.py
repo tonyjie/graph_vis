correct the computation of stress function; add some plotting

full_graph_sgd.py
@@ -157,10 +157,13 @@ def main(args):
 
     print(f"==== Training time: {end - start}; Step: {STEPS}; Device: {device} ====")
 
-    # === draw learning curve ===
-    fig, ax = plt.subplots()
-    ax.plot(np.linspace(0, STEPS, STEPS//LOG_INTERVAL), stress_rec)
-    plt.savefig(f"{os.path.dirname(args.input_file)}/learning_curve.png")
+    # # === draw learning curve ===
+    # fig, ax = plt.subplots()
+    # ax.plot(np.linspace(0, STEPS, STEPS//LOG_INTERVAL), stress_rec)
+    # ax.set_xlabel('Step')
+    # ax.set_ylabel('Stress')
+    # ax.set_yscale('log')
+    # plt.savefig(f"{os.path.dirname(args.input_file)}/learning_curve.png")
 
 
     if args.draw:

odgi_batch_sgd.py
@@ -8,11 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from odgi_dataset import OdgiTorchDataset, OdgiInterface
+import sys
 
-def draw_svg(x, gdata, output_name):
-    print("drawing visualization")
+def draw_svg(x, output_name):
+    # print("drawing visualization")
     # Draw SVG Graph with edge
-    ax = plt.axes()
+    # ax = plt.axes()
+    fig, ax = plt.subplots()
 
     xmin = x.min()
     xmax = x.max()
@@ -26,10 +28,34 @@ def draw_svg(x, gdata, output_name):
     # for i in range(gdata.get_node_count()):
     #     plt.text(np.mean(x[i,:,0]), np.mean(x[i,:,1]), i+1)
 
-    plt.savefig('output/' + output_name + '.png', format='png', dpi=1000)
+    plt.savefig(output_name, format='png', dpi=1000)
+
+def stress_fn(pos, dist):
+    '''
+    @brief: compute the overall stress given the positions and Distance Matrix.
+    @param: positions. [num_nodes, 2]
+    @param: dist: [num_paths, num_nodes, num_nodes]
+    @return: stress. 
+    '''
+
+    num_nodes = pos.shape[0]
+    copy1 = torch.reshape(pos, (1, num_nodes, 2))
+    copy2 = torch.reshape(pos, (num_nodes, 1, 2))
+    broadcasted1 = torch.broadcast_to(copy1, (num_nodes, num_nodes, 2))
+    broadcasted2 = torch.broadcast_to(copy2, (num_nodes, num_nodes, 2))
+    diff = broadcasted1 - broadcasted2
+    pred_dist = torch.norm(diff, dim=2).reshape((num_nodes, num_nodes))
+
+    mask = dist.ne(0)
+
+    # pred_dist = torch.where(mask, pred_dist, dist)
+    stress_matrix = torch.where(mask, torch.square((pred_dist - dist) / dist), dist) # [num_nodes, num_nodes]. Actually, this involves with redundant computation. (The matrix is symmetric)
+    stress = torch.sum(stress_matrix)
+
+    return stress
 
 
-def compute_stress(pos, Dist_paths):
+def compute_stress(pos, Dist_paths):# Wrong!!!
     '''
     @brief: compute the overall stress given the positions and Distance Matrix. 
     @param: positions. [num_nodes, 2]
@@ -49,7 +75,7 @@ def compute_stress(pos, Dist_paths):
     for i in range(num_paths):
         Dist = Dist_paths[i, :, :]
         mask = np.not_equal(Dist, 0)
-        pred_dist = np.where(mask, pred_dist, Dist)
+        pred_dist = np.where(mask, pred_dist, Dist) # This line will change pred_dist, which is NOT correct! 
         stress_matrix = np.where(mask, np.square((pred_dist - Dist) / Dist), Dist)
         stress += np.sum(stress_matrix)
     
@@ -65,11 +91,21 @@ def main(args):
     Dist_paths_arr = np.load(os.path.dirname(args.file) + '/Dist_paths.npy')
     print(f"==== Finish Loading Dist_paths_arr: {Dist_paths_arr.shape} ====") # [num_paths, num_nodes, num_nodes]
 
+    # breakpoint()
+    # change 1e-9 to 0. We don't compute these terms for stress. 
+    Dist_paths_arr[np.where(Dist_paths_arr == 1e-9)] = 0
+    dist = torch.tensor(Dist_paths_arr, dtype=torch.float32)
 
     # # ========== Load Graph, Get Constraints ============
     dataset = OdgiTorchDataset(args.file)
 
+    print(f"Steps_per_iter: {len(dataset)}")
+
+
     n = dataset.get_node_count()
+
+    print(f"node_count: {n}")
+    print(f"update per node: {len(dataset) / n}")
 
     w_min = dataset.w_min
     w_max = dataset.w_max
@@ -91,20 +127,25 @@ def main(args):
     for i in range(num_iter):
         schedule.append(eta(i))
 
-
+    stress_rec = np.zeros(num_iter + 1)
     # ========== Initialize the Positions ============
 
     x = torch.rand([n,2,2], requires_grad=True)
 
 
-    initial_stress = compute_stress(x.detach().numpy().reshape(n*2,2), Dist_paths_arr)
-    print(f"initial stress: {initial_stress}")
+    initial_stress = stress_fn(x.reshape(n*2, 2), dist)
+
+    # initial_stress = compute_stress(x.detach().numpy().reshape(n*2,2), Dist_paths_arr)
+    print(f"initial stress: {initial_stress:.2e}")
+    draw_svg(x.detach().numpy(), f"initial")
+    stress_rec[0] = initial_stress
 
     my_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)       # is always shuffled anyways
     # ========== Training ============
+    
     start = datetime.datetime.now()
     for idx_c, c in enumerate(schedule):
-        print("Computing iteration", idx_c+1, "of", num_iter)
+        # print("Computing iteration", idx_c+1, "of", num_iter)
         for batch_idx, (i, j, vis_p_i, vis_p_j, w, dis) in enumerate(my_dataloader): # batch size = 2
             # print(idx_c, ": ", batch_idx, " / ", dataset.__len__())
             # w_choose = torch.min(w) # choose the minimum w in the batch. This is different from the original paper. 
@@ -121,17 +162,33 @@ def main(args):
             x.data.sub_(lr * x.grad.data) # lr set to be a vector?
             x.grad.data.zero_()
 
-        stress = compute_stress(x.detach().numpy().reshape(n*2,2), Dist_paths_arr)
-        print(f"Iteration {idx_c+1} Done.  Stress: {stress}")
-            
+        x_np = x.detach().numpy()
+        draw_svg(x_np, f"{os.path.dirname(args.file)}/iter{idx_c}.png")
+        OdgiInterface.generate_layout_file(dataset.get_graph(), x_np, f"{os.path.dirname(args.file)}/iter{idx_c}.lay")
+
+        stress = stress_fn(x.reshape(n*2,2), dist)
+        # stress = compute_stress(x_np.reshape(n*2,2), Dist_paths_arr)
+        print(f"Iteration {idx_c+1}: Stress = {stress:.2e}")
+        stress_rec[idx_c+1] = stress
+
     end = datetime.datetime.now()
     print(f"Time: {end - start}")
 
     # Draw the Visualization Graph
-    x_np = x.detach().numpy()
+    # x_np = x.detach().numpy()
 
-    OdgiInterface.generate_layout_file(dataset.get_graph(), x_np, "output/lil.lay")
-    draw_svg(x_np, dataset, f"out")
+    # OdgiInterface.generate_layout_file(dataset.get_graph(), x_np, "output/lil.lay")
+    # draw_svg(x_np, f"out")
+
+    # === draw learning curve ===
+    fig, ax = plt.subplots()
+    ax.plot(stress_rec)
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Stress')
+    ax.set_yscale('log')
+    ax.set_title('Stress Curve: Pairwise Update')
+    plt.tight_layout()
+    plt.savefig(f"{os.path.dirname(args.file)}/stress_curve_odgi_batch_sgd.png")
 
 
 if __name__ == "__main__":

odgi_full_graph_sgd.py
@@ -100,8 +100,11 @@ class PlaceEngine(nn.Module):
         for i in range(num_paths):
             dist = Dist_paths[i, :, :]
             mask = dist.ne(0)
-            pred_dist = torch.where(mask, pred_dist, dist)
-            stress_matrix = torch.where(mask, torch.square((pred_dist - dist) / dist), dist) # [num_nodes, num_nodes]. Actually, this involves with redundant computation. (The matrix is symmetric)
+            pred_dist_mask = torch.where(mask, pred_dist, dist)
+            # pred_dist = torch.where(mask, pred_dist, dist)
+            # stress_matrix = torch.where(mask, torch.square(pred_dist - dist), dist) # no w as weight
+            stress_matrix = torch.where(mask, torch.square((pred_dist_mask - dist) / dist), dist) # w=1/d**2 setting     # [num_nodes, num_nodes]. Actually, this involves with redundant computation. (The matrix is symmetric)
+            # stress_matrix = torch.where(mask, torch.square(pred_dist - dist) / dist, dist)  # w = 1/d
             stress += torch.sum(stress_matrix)
         
         return stress
@@ -203,7 +206,7 @@ def main(args):
         mod.scheduler.step() # learning rate scheduler
 
         if i % LOG_INTERVAL == 0:
-            print(f"step {i}/{STEPS}: {stress}")
+            print(f"step {i}/{STEPS}: {stress:.2e}")
             stress_rec[i//LOG_INTERVAL] = stress.item()
 
         if args.draw and i % DRAW_INTERVAL == 0:
@@ -219,9 +222,14 @@ def main(args):
 
 
     # === draw learning curve ===
-    # fig, ax = plt.subplots()
-    # ax.plot(np.linspace(0, STEPS, STEPS//LOG_INTERVAL), stress_rec)
-    # plt.savefig(f"{os.path.dirname(args.input_file)}/learning_curve.png")
+    fig, ax = plt.subplots()
+    ax.plot(np.linspace(0, STEPS, STEPS//LOG_INTERVAL), stress_rec)
+    ax.set_xlabel('Step')
+    ax.set_ylabel('Stress')
+    ax.set_yscale('log')
+    ax.set_title('Stress Curve: Full Graph Update')
+    plt.tight_layout()
+    plt.savefig(f"{os.path.dirname(args.input_file)}/stress_curve_odgi_full_graph_sgd.png")
 
 
     if args.draw:

odgi_model.py
@@ -34,7 +34,8 @@ def compute_stress(pos, Dist_paths):
         Dist = Dist_paths[i, :, :]
         mask = np.not_equal(Dist, 0)
         pred_dist = np.where(mask, pred_dist, Dist)
-        stress_matrix = np.where(mask, np.square((pred_dist - Dist) / Dist), Dist)
+        # stress_matrix = np.where(mask, np.square((pred_dist - Dist) / Dist), Dist) # w = 1/(d**2)
+        stress_matrix = np.where(mask, np.square(pred_dist - Dist) / Dist, Dist)  # w = 1/d
         stress += np.sum(stress_matrix)
     
     return stress
@@ -57,15 +58,15 @@ def draw_svg(x, gdata, output_name):
     # for i in range(gdata.get_node_count()):
     #     plt.text(np.mean(x[i,:,0]), np.mean(x[i,:,1]), i+1)
 
-    plt.axis("off")
+    # plt.axis("off")
     plt.savefig(output_name + '.png', format='png', dpi=1000, bbox_inches="tight")
     plt.close()
 
 
 def main(args):
-    if (args.stress):
-        Dist_paths_arr = np.load(os.path.dirname(args.file) + '/Dist_paths.npy')
-        print(f"==== Finish Loading Dist_paths_arr: {Dist_paths_arr.shape} ====") # [num_paths, num_nodes, num_nodes]
+    # if (args.stress):
+    Dist_paths_arr = np.load(os.path.dirname(args.file) + '/Dist_paths.npy')
+    print(f"==== Finish Loading Dist_paths_arr: {Dist_paths_arr.shape} ====") # [num_paths, num_nodes, num_nodes]
 
 
     use_cuda = args.cuda and torch.cuda.is_available()
@@ -99,9 +100,9 @@ def main(args):
 
     x = torch.rand([n,2,2], dtype=torch.float64, device=device)
 
-    if (args.stress):
-        initial_stress = compute_stress(x.cpu().detach().numpy().reshape(n*2,2), Dist_paths_arr)
-        print(f"initial stress: {initial_stress:.2e}")
+    # if (args.stress):
+    initial_stress = compute_stress(x.cpu().detach().numpy().reshape(n*2,2), Dist_paths_arr)
+    print(f"initial stress: {initial_stress:.2e}")
 
 
     start = time.time()
@@ -133,7 +134,8 @@ def main(args):
             w = 1 / dis
 
             mu = eta * w
-            mu_m = torch.min(mu, torch.ones_like(mu))
+            # mu_m = mu
+            mu_m = torch.min(mu, torch.ones_like(mu)) # capping mu to 1
 
             x_i = x[i-1,vis_p_i,0]
             x_j = x[j-1,vis_p_j,0]
@@ -163,9 +165,9 @@ def main(args):
             x[i-1, vis_p_i, 1] = x[i-1, vis_p_i, 1] - r_y
             x[j-1, vis_p_j, 1] = x[j-1, vis_p_j, 1] + r_y
 
-            # if (torch.isnan(x).any()):
-            #     # breakpoint()
-            #     raise ValueError(f"Iter[{iteration}] Step[{batch_idx}]. x: {x} is NaN. mag: {mag}")
+            if (torch.isnan(x).any()):
+                breakpoint()
+                raise ValueError(f"Iter[{iteration}] Step[{batch_idx}]. x: {x} is NaN. mag: {mag}. mu_m: {mu_m}. ")
 
             if (device == torch.device("cuda")):
                 torch.cuda.synchronize()
@@ -187,9 +189,14 @@ def main(args):
     dataload_time = overall_time - compute_time - transfer_time
     print(f"Overall time {overall_time:.2f} sec; Dataloading time: {dataload_time:.2f} sec; Computation time: {compute_time:.2f} sec; Data Transfer time: {transfer_time:.2f} sec")
 
-    # x_np = x.cpu().detach().numpy()
-    # OdgiInterface.generate_layout_file(data.get_graph(), x_np, os.path.dirname(args.file) + "/layout.lay")
-    # draw_svg(x_np, data, f"{os.path.dirname(args.file)}/out_final")
+    # final stress
+    stress = compute_stress(x.cpu().detach().numpy().reshape(n*2,2), Dist_paths_arr)
+    print(f"Final Stress: {stress:.2e}")
+
+
+    x_np = x.cpu().detach().numpy()
+    OdgiInterface.generate_layout_file(data.get_graph(), x_np, os.path.dirname(args.file) + "/layout.lay")
+    draw_svg(x_np, data, f"{os.path.dirname(args.file)}/out_final")
 
 
 if __name__ == "__main__":